e481f633dd1eceda2bf0e492edd88e341fa29150
Vyshnavivyshu444 <dvyshnavi572@gmail.com>
2026-06-08 20:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (18.96%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #Two Sum 
+        nums_hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in nums_hash: 
+                return [nums_hash[target - nums[i]], i]
+            nums_hash[nums[i]] = i
+        
